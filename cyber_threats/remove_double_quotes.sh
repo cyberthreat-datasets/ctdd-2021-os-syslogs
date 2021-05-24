0f3404323eb20ea59810b9f71249372dbed3ce90
Anon <ctdd@gmail.com>
2021-05-24 06:33:15 +0000

    Change Dataset Directories

--- cyber_threats/remove_double_quotes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for server in $(ls | grep syslog_log); do cat $server | sed -e 's/^"//' -e 's/"$//' > $server-temp; done
-rm *syslog_log
-for server in $(ls | grep syslog_log); do mv $server $(echo "$server" | awk -F_ '{print $1 FS $2 FS "syslog_log"}'); done

d65947994e4789c9c01518e6f1b2d144a7e548ca
Anon <ctdd@gmail.com>
2021-05-24 06:31:48 +0000

    Change Dataset Directories

---
+++ CTDD_Dataset/cyber_threats/remove_double_quotes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for server in $(ls | grep syslog_log); do cat $server | sed -e 's/^"//' -e 's/"$//' > $server-temp; done
+rm *syslog_log
+for server in $(ls | grep syslog_log); do mv $server $(echo "$server" | awk -F_ '{print $1 FS $2 FS "syslog_log"}'); done
